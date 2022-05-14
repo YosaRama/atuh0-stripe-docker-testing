baec351cc3c39d59a87f4c36222bfae08602a77b
Yosa Rama <yosamelody07@gmail.com>
2022-05-14 16:41:56 +0800

    ADD : Next Auth Secret

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -8,6 +8,7 @@ export default NextAuth({
   session: {
     strategy: "jwt",
   },
+  secret: "uu2DXTo8CggeMp2gG9xlX0DYw9yNEFz1Z/9P4p3wRdc=",
   providers: [
     Auth0Provider({
       clientId: process.env.AUTH0_CLIENT_ID,
